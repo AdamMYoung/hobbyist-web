b7a71c839d1ff7ac326431790af73cc8b8382b75
Adam Young <adamyoungg97@gmail.com>
2020-12-19 01:53:08 +0000

    Fixed SEO of not found page

--- src/routes/not-found/index.tsx
+++ src/routes/not-found/index.tsx
@@ -1,22 +1,27 @@
+import { useHistory } from 'react-router-dom';
+
 import SplitPage from '../../views/split-page';
 import Button from '../../components/button';
-import { useHistory } from 'react-router-dom';
+import SEO from '../../components/seo';
 
 const NotFound = () => {
     const history = useHistory();
 
     return (
-        <SplitPage>
-            <SplitPage.Body>
-                <SplitPage.Center>
-                    <p className="text-2xl font-bold text-center">Page Not Found</p>
+        <>
+            <SEO title="Page Not Found" />
+            <SplitPage>
+                <SplitPage.Body>
+                    <SplitPage.Center>
+                        <p className="text-2xl font-bold text-center">Page Not Found</p>
 
-                    <Button className="mx-auto mt-4" variant="primary" onClick={() => history.replace('/')}>
-                        Home
-                    </Button>
-                </SplitPage.Center>
-            </SplitPage.Body>
-        </SplitPage>
+                        <Button className="mx-auto mt-4" variant="primary" onClick={() => history.replace('/')}>
+                            Home
+                        </Button>
+                    </SplitPage.Center>
+                </SplitPage.Body>
+            </SplitPage>
+        </>
     );
 };
 
