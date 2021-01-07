7cc810a91358cd34eb3eae889440f08cbc72a2bb
Adam Young <adamyoungg97@gmail.com>
2021-01-07 15:52:25 +0000

    Added CRA4 service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import App from './App';
@@ -17,4 +16,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-serviceWorker.register();
