cc007164e13c3de1ad7723d6d8e49aeeaa2d13e5
Adam Young <adamyoungg97@gmail.com>
2021-01-14 17:25:43 +0000

    Improved fade of modal

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -20,10 +20,9 @@ const Modal: React.FC<Props> = (props) => {
 
     return (
         <>
-            <div className="fixed z-50 top-0 left-0 w-full h-full bg-gray-400 opacity-70 pointer-events-none" />
-
             <LoadTransition className="fixed flex flex-col justify-center z-50 top-0 left-0 w-full h-full">
-                <div className="m-auto rounded-lg border bg-white p-4 w-100 opacity-100">
+                <div className="fixed z-40 top-0 left-0 w-full h-full bg-gray-400 opacity-70 pointer-events-none" />
+                <div className="m-auto z-50 rounded-lg border bg-white p-4 w-100 opacity-100">
                     <section>
                         {title && <p className="text-2xl font-bold mb-2">{title}</p>}
                         {children}
