0c7bbbd9cdd30c1ed2b28494eabee3b711bc60a3
Adam Young <adamyoungg97@gmail.com>
2021-01-25 23:57:05 +0000

    Added cropping modals

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -19,17 +19,15 @@ const Modal: React.FC<Props> = (props) => {
     }, [setLocked]);
 
     return (
-        <>
-            <LoadTransition className="fixed flex flex-col justify-center z-50 top-0 left-0 w-full h-full">
-                <div className="fixed z-40 top-0 left-0 w-full h-full bg-gray-400 opacity-70 pointer-events-none" />
-                <div className="m-auto z-50 rounded-lg border bg-white p-4 w-100 opacity-100">
-                    <section>
-                        {title && <p className="text-2xl font-bold mb-2">{title}</p>}
-                        {children}
-                    </section>
-                </div>
-            </LoadTransition>
-        </>
+        <LoadTransition className="fixed flex flex-col justify-center z-50 top-0 left-0 p-2 sm:p-4 h-full">
+            <div className="fixed z-40 top-0 left-0 w-full h-full bg-gray-400 opacity-70 pointer-events-none" />
+            <div className="justify-center z-50 rounded-lg border bg-white p-4 opacity-100 max-w-6xl">
+                <section>
+                    {title && <p className="text-2xl font-bold mb-2">{title}</p>}
+                    {children}
+                </section>
+            </div>
+        </LoadTransition>
     );
 };
 
